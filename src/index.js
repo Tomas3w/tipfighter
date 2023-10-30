8fcf07c1cbd19198627f795c7f9add6902bd6758
Gonzalo Fontana <joacomaidana11@gmail.com>
2023-10-30 00:00:33 -0300

    video 4, animaciones, forward backward con estado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import {Ken} from "./entities/fighters/Ken.js";
 import {Ryu} from "./entities/fighters/Ryu.js";
 import {Stage} from "./entities/Stage.js";
 import {FpsCounter} from "./entities/FpsCounter.js";
-
+import {STAGE_FLOOR} from "./constants/stage.js";
+import { FighterDirection } from "./constants/fighter.js";
 //multiplicamos por una constante para escalar el lienzo basado en street fighter
 const GameViewport = {
     WIDTH: 384,
@@ -14,28 +15,32 @@ const GameViewport = {
 window.addEventListener('load', function (){
     const canvasEl = document.querySelector('canvas');
     const context = canvasEl.getContext('2d');
-
+    context.imageSmoothingEnabled = false;
     
     canvasEl.width = GameViewport.WIDTH;
     canvasEl.height = GameViewport.HEIGHT;
     const entities = [ 
         new Stage(),
-        new Ken(80,110,150),
-        new Ryu(80,110,-150),
+        new Ken(104,STAGE_FLOOR,FighterDirection.LEFT),
+        new Ryu(280,STAGE_FLOOR,FighterDirection.RIGHT),
         new FpsCounter(),
     ]
    
+    let frameTime = {
+         previous : 0,
+         secondsPassed : 0,
+    };
 
-    let previusTime = 0;
-    let secondsPassed = 0;
 
+  
     function frame(time){
         window.requestAnimationFrame(frame);
-        secondsPassed = (time - previusTime) / 1000;
-        previusTime = time ;
-
+        frameTime = {
+            secondsPassed : (time - frameTime.previous) / 1000,
+            previous : time,
+        }
         for (const entity of entities){
-            entity.update(secondsPassed,context);
+            entity.update(frameTime,context);
 
         }
 
